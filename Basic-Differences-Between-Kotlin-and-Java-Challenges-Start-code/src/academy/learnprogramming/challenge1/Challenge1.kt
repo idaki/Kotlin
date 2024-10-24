fbc0d49d5482be4f1862b722104ab8b8013412a9
Idaki Milev <idaki@me.com>
2024-10-24 11:06:06 +0300

    kotlin basic

---
+++ Basic-Differences-Between-Kotlin-and-Java-Challenges-Start-code/src/academy/learnprogramming/challenge1/Challenge1.kt
@@ -0,0 +1,24 @@
+package academy.learnprogramming.challenge1
+
+fun main(args: Array<String>) {
+    val hello1 = "Hello"
+    val hello2 = "Hello"
+
+    println("hello1 s referentially equal to hello2: ${hello1 === hello2}")
+
+    println("hello1 s structurally equal to hello2: ${hello1 == hello2}")
+
+    var int = 2988
+
+    val text: Any = "Any type is the root of the Kotlin class hierarchy"
+  if (text is String) {
+      println(text.toUpperCase())
+  }
+
+
+    println("""     1   
+                 |  11
+                 | 111
+                 |1111""".trimIndent())
+}
+

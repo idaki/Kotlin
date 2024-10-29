7fdd675d36714fa0edee3652a25d92efc3b9c864
Idaki Milev <idaki@me.com>
2024-10-29 09:44:01 +0200

    OO challenges done

--- Udemy/OO-and-Kotlin-Classes,-Functions,-Inheritance-Challenges,-Part-One-Start-code/src/academy/learnprogramming/oochallenge/OOChallenge.kt
+++ Udemy/OO-and-Kotlin-Classes,-Functions,-Inheritance-Challenges,-Part-One-Start-code/src/academy/learnprogramming/oochallenge/OOChallenge.kt
@@ -1,8 +1,11 @@
 package academy.learnprogramming.oochallenge
 
- open class KotlinBicycle  (var cadence: Int,var speed: Int ,var gear:Int )
+ open class KotlinBicycle  (var cadence: Int,var speed: Int ,var gear:Int =5 )
+
 
  {
+
+
      fun applyBrake(decrement: Int) {
          speed -= decrement
      }
@@ -20,9 +23,14 @@ package academy.learnprogramming.oochallenge
  }
 
 
-class KotlinMountainBike (var seatHeight: Int, cadence: Int, speed: Int , gear:Int) :
+class KotlinMountainBike (var seatHeight: Int, cadence: Int, speed: Int , gear:Int=5) :
 KotlinBicycle(cadence, speed, gear) {
+    constructor (seatHeight: Int, cadence: Int, speed: Int , gear: Int =5, color: String ):
+            this(cadence, cadence, speed, gear) {}
 
+    companion object {
+        val availableColor= listOf("red", "green", "blue")
+    }
     override fun printDescription(){
     super.printDescription()
     println(" Mountain bike is in gear $gear")
@@ -30,7 +38,7 @@ KotlinBicycle(cadence, speed, gear) {
 }
 }
 
-class KotlinRoadBike (val tireWidth: Int, cadence: Int, speed: Int, gear:Int) :
+class KotlinRoadBike (val tireWidth: Int, cadence: Int, speed: Int, gear:Int=5) :
         KotlinBicycle(cadence, speed, gear) {
 
     override fun printDescription(){
@@ -39,3 +47,24 @@ class KotlinRoadBike (val tireWidth: Int, cadence: Int, speed: Int, gear:Int) :
     }
         }
 
+
+fun main(args: Array<String>) {
+
+    val kotlinBicycle =  KotlinBicycle(50,200,6)
+    val kotlinMountainBike = KotlinMountainBike(30, 70,155,4)
+    val kotlinRoadBike = KotlinRoadBike(40, 60,250,4)
+
+    val kotlinBicycle2 =  KotlinBicycle(50,200)
+    val kotlinMountainBike2 = KotlinMountainBike(30, 70,155)
+    val kotlinRoadBike2 = KotlinRoadBike(40, 60,250)
+
+    val kotlinMountainBike3 = KotlinMountainBike(30, 70,55, color="Red")
+KotlinMountainBike.availableColor.forEach { println(it) }
+    kotlinBicycle.printDescription()
+    kotlinMountainBike.printDescription();
+    kotlinRoadBike.printDescription();
+
+    kotlinBicycle2.printDescription();
+    kotlinMountainBike2.printDescription();
+    kotlinRoadBike2.printDescription();
+}
